b554ddf675ca80565bd0a21509233061edc48681
Avneet Singh <avneets2103@gmail.com>
2024-03-03 06:50:32 +0530

    small changes

--- src/Components/LowerLayer/Screen3/LScreen3.tsx
+++ src/Components/LowerLayer/Screen3/LScreen3.tsx
@@ -8,7 +8,7 @@ export default function LScreen3() {
                 <Name colorMode="black" />
             </div>
             <div className="endLinks">
-                <a href="">
+                <a href="https://github.com/avneets2103/parodyPortfolio" target="_blank">
                 <p>Source code</p>
                 <img
                     src="/icons/up-right2.png"
@@ -17,7 +17,7 @@ export default function LScreen3() {
                     height={10}
                 />
                 </a>
-                <a href="https://linktr.ee/avneets2103?utm_source=linktree_admin_share">
+                <a href="https://linktr.ee/avneets2103?utm_source=linktree_admin_share" target="_blank">
                     <p>Link tree</p>
                     <img
                     src="/icons/up-right2.png"
@@ -26,7 +26,7 @@ export default function LScreen3() {
                     height={10}
                 />
                 </a>
-                <a href="https://drive.google.com/file/d/1xRMhLV0Nu6336UBwHtFIpIoGg9O8dJ1o/view?usp=sharing">
+                <a href="https://drive.google.com/file/d/1xRMhLV0Nu6336UBwHtFIpIoGg9O8dJ1o/view?usp=sharing" target="_blank">
                     <p>Resume</p>
                     <img
                     src="/icons/up-right2.png"
