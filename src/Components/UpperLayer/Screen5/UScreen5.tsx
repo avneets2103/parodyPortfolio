b67a28e2fdb8be68e640f2c66a1c7e915ccb9ef0
Avneet Singh <avneets2103@gmail.com>
2024-03-11 15:47:48 +0530

    imports

--- src/Components/UpperLayer/Screen5/UScreen5.tsx
+++ src/Components/UpperLayer/Screen5/UScreen5.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './UScreen5.css'
 import useWindowDimensions from "../../windowDim/windowDim";
-import { mobileTreshold, tabletTreshold } from '../../../CONSTANTS';
 
 interface project {
     Pid: string,
